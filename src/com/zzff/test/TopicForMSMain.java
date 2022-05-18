a7b0804080942dfd21c57cee00d7490f724c5faf
commsoft <jiang1557494@163.com>
2022-05-18 14:59:45 +0800

    master测试第二次提交

--- src/com/zzff/test/TopicForMSMain.java
+++ src/com/zzff/test/TopicForMSMain.java
@@ -5,5 +5,6 @@ public class TopicForMSMain {
     public static void main(String[] args) {
         System.out.println("master 第一次提交");
         System.out.println("hotfix 第一次提交");
+        System.out.println("master 第二次提交");
     }
 }
